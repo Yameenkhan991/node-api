380fae6b47c0a15319922df8890fd6835e08105c
Toshiba <yameenk991@gmail.com>
2023-04-12 23:11:59 -0700

    first projrct

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+
 const express = require ('express')
+const mongoos = require ("mongoose")
 const app = express ()
 
 //routes
 app.get ('/', (req, res)=>{
-   res.send('hello node api') 
+   res.send('hello node api , this is my first app') 
 })
-app.listen(3000, ()=>{
-    console.log('node api is running on port 3000')
+app.get ('/blog', (req, res)=>{
+   res.send('hello blog my name is yameen') 
+})
+
+
+
+mongoos.connect("mongodb+srv://admin:pj5ZOjGFVY14b1jx@yameen.pvpnzci.mongodb.net/node-api?retryWrites=true&w=majority")
+.then(()=>{
+   console.log('connected to mongodb')
+   app.listen(3000, ()=>{
+   console.log('node api is running on port 3000')
+
+})
+  
+})
+.catch((error)=>{
+   console.log('error')
 
 })
